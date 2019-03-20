363b63ee299e44d22ffb3081801fa380fa7c9156
Patrick R. Wright <patrick.wright@usb.ch>
2019-03-20 15:17:29 +0100

    internals file name must have been causing issues

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,7 +1,7 @@
 library(testthat)
 library(secuTrialR)
 
-test_check("internals")
+test_check("internal_functions")
 test_check("load_export_options")
 test_check("load_export_table")
 test_check("id_conversion")
