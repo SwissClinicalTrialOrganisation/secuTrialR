dd626b80ec4f8ce089e05b2c6b78880726e312ac
Patrick R. Wright <patrick.wright@usb.ch>
2019-03-20 15:52:29 +0100

    changing test_check to only the package name otherwise travis seems to have problems

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,8 +1,4 @@
 library(testthat)
 library(secuTrialR)
 
-test_check("internal_functions")
-test_check("load_export_options")
-test_check("load_export_table")
-test_check("id_conversion")
-test_check("load_secuTrial_export")
+test_check("secuTrialR")
