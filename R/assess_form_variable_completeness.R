8a1512cd23bdb99355a031e42ac14f8632f846a9
Patrick R. Wright <patrick.wright@usb.ch>
2019-03-21 11:28:22 +0100

    adding assessment of var completeness

---
+++ R/assess_form_variable_completeness.R
@@ -0,0 +1,137 @@
+#'
+#' Asses completeness of data for each variable in a secuTrial export
+#'
+#' @description
+#' Variable completeness is defined as percentage of data entered for a variable
+#' when considering all occurrences of the variable throughout the visit plan and
+#' all patients/cases registered in the secuTrial data base. The completeness can
+#' be assessed for 'allforms', which will also take unsaved forms into account, and
+#' for 'savedforms' which will only consider forms that have been actively saved.
+#' In order to asses variable completeness the function requires a loaded secuTrial
+#' validation overview and a loaded secuTrial standard export. Both the validation
+#' overview and the data export should be from the same time (not more than minutes
+#' apart if possible) to ensure data integrity.
+#' Variable completeness is considered based on rules in the secuTrial setup. Thus,
+#' if a variable is not marked as necessary for completeness, this variable will
+#' always be considered as 100 percent complete if only 'savedforms' are assessed.
+#'
+#' @export assess_form_variable_completeness
+#'
+#' @param form data.frame Form for which to assess variable completeness
+#'             (i.e. a list element returned by load_secuTrial_export).
+#' @param patient_table data.frame The central patient record file
+#'                      (i.e. 'patient' from the list returned by load_secuTrial_export).
+#' @param validation_overview tibble returned by load_validation_overview.
+#' @param completeness string Specifies if completeness is assessed for all forms ('allforms')
+#'                     or only for saved forms ('savedforms').
+#' @param occ_in_vp integer Specifies how often the form occurs in the visit plan
+#'                  (only relevant if completeness = 'allforms' is set).
+#' @param omit.mnp boolean Removes variable names from the result table that start with mnp.
+#' @return data.frame showing percent completeness per variable.
+#'
+#' @examples
+#' \donttest{
+#' assess_form_variable_completeness(form = sT_export$bmd, patient_table = sT_export$patient,
+#'                                   validation_overview = val_ovv, completeness = "allforms",
+#'                                   occ_in_vp = 5)
+#' }
+#'
+#' @seealso load_validation_overview, load_secuTrial_export
+#'
+assess_form_variable_completeness <- function(form,
+                                              patient_table,
+                                              validation_overview,
+                                              completeness = "allforms",
+                                              occ_in_vp = 1,
+                                              omit.mnp = TRUE) {
+  # form must appear at least once in visit plan
+  if (occ_in_vp < 1) {
+    stop(paste("occ_in_vp is set to", occ_in_vp, "but can not be smaller than 1."))
+  }
+  # occ_in_vp is obsolete if "savedforms" is specified
+  if (occ_in_vp > 1 & completeness == "savedforms") {
+    warning(paste("occ_in_vp set to", occ_in_vp, "ignored because completeness has been set to 'savedforms'."))
+  }
+  # check if c("Completion status", "Column") are available in the validation overview they contain
+  # the key data to make the assessment and are not checked by default in secuTrial
+  if (! all(c("Completion status", "Column") %in% names(validation_overview))) {
+    stop(paste("'Completion status' and 'Column' are not available in the supplied validation overview but are needed.",
+               "Reexport your validation overview in secuTrial with these two columns displayed.'"))
+  }
+
+  if (completeness == "allforms") {
+    # count cases (all registered ids)
+    forms_per_case_count <- length(patient_table[, 1])
+    # multiply forms_per_case_count with form occurence in
+    # visit plan to adjust for repeating forms
+    forms_per_case_count <- forms_per_case_count * occ_in_vp
+    # count form entries
+    saved_form_count <- length(form[, 1])
+    # count empty forms - frozen forms appear as "rule-based freeze"
+    # and are thus not counted as missing
+    empty_form_count <- forms_per_case_count - saved_form_count
+    # consistency check, empty_form_count cannot be negative
+    if (empty_form_count < 0) {
+      stop(paste0("You have likely not specified occ_in_vp correctly. You specified: ", occ_in_vp))
+    }
+    # form table
+    form_table <- table(names(form))
+    # init empty counts
+    form_table[names(form_table)] <- empty_form_count
+    # init table_missing_counts
+    table_missing_counts <- form_table
+    # validation overview as table
+    validation_overview_table <- table(validation_overview$Column)
+    if (length(validation_overview_table) != 0) {
+      # missing count per variable
+      table_missing_counts[names(validation_overview_table)] <-
+        table_missing_counts[names(validation_overview_table)] +
+        validation_overview_table[names(validation_overview_table)]
+    }
+    # change to dataframe
+    table_missing_counts <- as.data.frame(as.matrix(table_missing_counts))
+    # add completeness
+    table_missing_counts$completeness <- 1 - (table_missing_counts$V1 / forms_per_case_count)
+    # table names and formatting
+    table_missing_counts <- rownames_to_column(table_missing_counts)
+    names(table_missing_counts) <- c("variable", "timesmissing", "completeness")
+    # validation report also has entries for other forms
+    # which return double NA and can be removed
+    non_na_idx <- which(! (is.na(table_missing_counts$timesmissing) &
+                           is.na(table_missing_counts$completeness))
+                       )
+    table_missing_counts <- table_missing_counts[non_na_idx, ]
+    # add timesentered column
+    table_missing_counts$timesentered <- as.vector(colSums(! is.na(form[, table_missing_counts$variable])))
+    table_missing_counts <- table_missing_counts[, c("variable", "timesentered", "timesmissing", "completeness")]
+  } else if (completeness == "savedforms") {
+
+    # count form entries
+    saved_form_count <- length(form[, 1])
+    # validation overview as table
+    validation_overview_table <- table(validation_overview$Column)
+    if (length(validation_overview_table) == 0) {
+      return("100% completeness. Your Validation overview is empty.")
+    }
+    # missing count per variable
+    table_missing_counts <- na.omit(as.data.frame(validation_overview_table[names(form)]))
+    # add completeness
+    table_missing_counts$completeness <- 1 - (table_missing_counts$Freq / saved_form_count)
+    # table names and formatting
+    names(table_missing_counts) <- c("variable", "timesmissing", "completeness")
+    # add timesentered column
+    table_missing_counts$timesentered <- as.vector(colSums(! is.na(form[, as.character(table_missing_counts$variable)])))
+    table_missing_counts <- table_missing_counts[, c("variable", "timesentered", "timesmissing", "completeness")]
+  } else {
+    stop(paste0("completeness argument must be 'allforms' or 'savedforms'. You specified: ", "'", completeness, "'"))
+  }
+
+  # remove variables starting with mnp
+  if (omit.mnp) {
+    mnp_idx <- grep("^mnp", table_missing_counts$variable)
+    if (length(mnp_idx) > 1) {
+        table_missing_counts <- table_missing_counts[-mnp_idx, ]
+    }
+  }
+  return(table_missing_counts)
+}
---
+++ tests/testthat/test-assess_form_variable_completeness.R
@@ -0,0 +1,66 @@
+context("assess variable completeness")
+
+# load exports
+short_export_location <- system.file("extdata",
+                                     "s_export_CSV-xls_BMD.zip",
+                                     package = "secuTrialR")
+
+long_export_location <- system.file("extdata",
+                                    "s_export_CSV-xls_longnames_BMD.zip",
+                                    package = "secuTrialR")
+
+sT_export_short <- load_secuTrial_export(data_dir = short_export_location)
+sT_export_long <- load_secuTrial_export(data_dir = long_export_location)
+
+# load validation overview
+val_ovv_location <- system.file("extdata", "bmd_validation_overview.xlsx", package = "secuTrialR")
+val_ovv <- load_validation_overview(data_dir = val_ovv_location)
+
+# test error handling
+test_that("Exceptions correctly triggered.", {
+  expect_error(assess_form_variable_completeness())
+  expect_error(assess_form_variable_completeness(occ_in_vp = 0))
+  expect_error(assess_form_variable_completeness(validation_overview = val_ovv, completeness = "yes"))
+})
+
+# bmd completeness allforms short and long
+completeness_bmd_short <- assess_form_variable_completeness(form = sT_export_short$bmd,
+                                                            patient_table = sT_export_short$patient,
+                                                            validation_overview = val_ovv,
+                                                            completeness = "allforms",
+                                                            occ_in_vp = 5)
+
+completeness_bmd_long <- assess_form_variable_completeness(form = sT_export_long$dem00bmd,
+                                                           patient_table = sT_export_long$patient,
+                                                           validation_overview = val_ovv,
+                                                           completeness = "allforms",
+                                                           occ_in_vp = 5)
+
+# test outputs allforms
+test_that("Variable completeness allforms working.", {
+  expect_equal(completeness_bmd_short, completeness_bmd_long)
+  expect_equal(dim(completeness_bmd_short), c(6, 4))
+  expect_equal(sum(completeness_bmd_long$timesentered), 3019)
+  expect_equal(sum(completeness_bmd_long$timesmissing), 371)
+  expect_equal(round(completeness_bmd_long$completeness, digits = 3), c( 0.890, 0.888, 0.892, 0.888, 0.892, 0.892))
+})
+
+# bmd completeness savedforms short and long
+completeness_bmd_short_saved <- assess_form_variable_completeness(form = sT_export_short$bmd,
+                                                                  patient_table = sT_export_short$patient,
+                                                                  validation_overview = val_ovv,
+                                                                  completeness = "savedforms")
+
+completeness_bmd_long_saved <- assess_form_variable_completeness(form = sT_export_long$dem00bmd,
+                                                                 patient_table = sT_export_long$patient,
+                                                                 validation_overview = val_ovv,
+                                                                 completeness = "savedforms")
+
+# test output savedforms
+test_that("", {
+  expect_equal(completeness_bmd_short_saved, completeness_bmd_long_saved)
+  expect_equal(dim(completeness_bmd_short_saved), c(3, 4))
+  expect_equal(sum(completeness_bmd_short_saved$timesentered), 1507)
+  expect_equal(sum(completeness_bmd_short_saved$timesmissing), 5)
+  expect_equal(round(completeness_bmd_short_saved$completeness, digits = 3), c(0.998, 0.996, 0.996))
+})
