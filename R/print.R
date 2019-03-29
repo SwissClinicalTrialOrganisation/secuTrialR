a254f9e4f966ff81293100cf2d063346ffaf013a
aghaynes <aghaynes@gmail.com>
2019-03-29 14:20:20 +0100

    Add a print method so that it doesnt print the whole data

---
+++ R/print.R
@@ -0,0 +1,22 @@
+#' print method
+#'
+#' @param x
+#'
+#' @return
+#' @export
+#'
+#' @examples
+print.secuTrialdata <- function(x){
+
+  print(paste("SecuTrial data imported from", x$export_options$data_dir))
+
+
+
+}
+
+
+
+
+
+
+
