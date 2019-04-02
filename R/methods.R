b4b83e4f56da49355c09d7f3bfddac1a8cd1ae20
aghaynes <aghaynes@gmail.com>
2019-04-02 11:49:16 +0200

    correct visit_structure to allow shortnames, move methods so that help files work

--- R/methods.R
+++ R/methods.R
@@ -43,26 +43,5 @@ plot.secuTrialdata <- function(x){
 
 
 
-#' @rdname visit_structure
-#' @usage plot(x)
-#' @export
-#' @examples
-#'   vs <- visit_structure(sT_export)
-#'   plot(vs)
-#'   # or, equivalently
-#'   plot(sT_export)
-
-plot.secuTrialvisit <- function(x){
-  # construct the figure
-  z <- !is.na(as.matrix(r[, grepl("tmpvar", names(r))]))
-  names <- gsub("tmpvar.", "", names(r[, grepl("tmpvar", names(r))]))
-  paropts <- par()
-  on.exit(paropts)
-  par(mai = c(0,0,0.1,0.1))
-  layout(matrix(c(0,1,0,0), 2, 2, byrow = TRUE))
-  image(t(z), yaxt = "n", xaxt = "n", col = c("white", "black"))
-  axis(2, r$formname, at = 0:(nrow(r)-1)/(nrow(r)-1), las = 1)
-  axis(1, names, at = 0:(length(names)-1)/(length(names)-1), las = 2)
-}
 
 
--- R/visit_structure.R
+++ R/visit_structure.R
@@ -17,20 +17,24 @@
 #' visit_structure(sT_export)
 #'
 visit_structure <- function(x){
-  tmp <- merge(x$visitplan, x$visitplanforms)
-  tmp <- merge(tmp, x$forms)
+  vp <- x[[x$export_options$meta_names$visitplan]]
+  if(any(is.na(vp$mnpvsno))) stop("Visits do not appear to be a part of this database")
+  vpf <- x[[x$export_options$meta_names$visitplanforms]]
+  f <- x[[x$export_options$meta_names$forms]]
+  tmp <- merge(vp, vpf, by = "mnpvisid")
+  tmp <- merge(tmp, f, by = "formid")
   u <- unique(tmp[, c("mnpvislabel", "formname")])
   u$tmpvar <- 1
   r <- reshape(u, direct = "wide",
                timevar = "mnpvislabel",
                idvar = "formname", v.names = "tmpvar")
   # column order
-  tmpl <- split(x$visitplan, x$visitplan$mnpvsno)[[1]]
+  tmpl <- split(vp, vp$mnpvsno)[[1]]
   tmpl$r <- 1:nrow(tmpl)
   neworder <- c(1, tmpl$r[match(tmpl$mnpvislabel, gsub("tmpvar.", "", names(r)[2:ncol(r)]))]+1)
   r <- r[, neworder]
   # row order
-  tmpl <- split(x$forms, x$forms$mnpvslbl)[[1]]
+  tmpl <- split(f, f$mnpvslbl)[[1]]
   f <- as.character(tmpl$formname)
   neworder <- na.exclude((1:length(f))[match(f, r$formname)])
   ro <- r[neworder, ]
@@ -39,5 +43,28 @@ visit_structure <- function(x){
 
   class(ro) <- c("secuTrialvisit", "data.frame")
   return(ro)
+}
+
+
+#' @rdname visit_structure
+#' @usage plot(x)
+#' @export
+#' @examples
+#'   vs <- visit_structure(sT_export)
+#'   plot(vs)
+#'   # or, equivalently
+#'   plot(sT_export)
+
+plot.secuTrialvisit <- function(x){
+  # construct the figure
+  z <- !is.na(as.matrix(r[, grepl("tmpvar", names(r))]))
+  names <- gsub("tmpvar.", "", names(r[, grepl("tmpvar", names(r))]))
+  paropts <- par()
+  on.exit(paropts)
+  par(mai = c(0,0,0.1,0.1))
+  layout(matrix(c(0,1,0,0), 2, 2, byrow = TRUE))
+  image(t(z), yaxt = "n", xaxt = "n", col = c("white", "black"))
+  axis(2, r$formname, at = 0:(nrow(r)-1)/(nrow(r)-1), las = 1)
+  axis(1, names, at = 0:(length(names)-1)/(length(names)-1), las = 2)
 }
 
