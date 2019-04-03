b6a5573037ce7c0bee2f404964625a0cc2244891
aghaynes <aghaynes@gmail.com>
2019-04-03 08:38:56 +0200

    add helpfiles

--- R/factorize.R
+++ R/factorize.R
@@ -1,7 +1,24 @@
+#' Add factors to secuTrialdata objects
+#'
+#' @rdname factor
+#' @name factor
+#' @param x a \code{secuTrialdata} object
+#' @return
+#' @export
+#'
+#' @examples
+
 # create factors
 factorize_secuTrial <- function(x, ...) UseMethod("factorize_secuTrial", x)
 
-# secuTrialdata method
+#' Method for secuTrialdata objects
+#' @rdname factor
+#' @param object a \code{secuTrialdata} object
+#'
+#' @return object with extra variables in forms for factors (names are appended with \code{.factor})
+#' @export
+#'
+#' @examples
 factorize_secuTrial.secuTrialdata <- function(object){
   if(!object$export_options$refvals_separate) stop("Reference values not available. Save reference values to seperate table in export")
   x <- object$export_options$data_names
@@ -18,6 +35,15 @@ factorize_secuTrial.secuTrialdata <- function(object){
   object
 }
 
+#' Method for data.frame objects
+#' @rdname factor
+#' @param data a \code{data.frame}, usually from within a \code{secuTrialdata} object
+#' @param cl a \code{data.frame}, usually taken from a \code{secuTrialdata} object, containing at least three variables - 1) \code{column} containing information on which form and variable is to be factorized (formatted as form.variable); 2) \code{code} containing the options that the variable can take as a number; 3) \code{value} contains the text relating to the \code{code} number
+#' @param form which form you are currently working on (used for filtering \code{cl})
+#'
+#' @export
+#'
+#' @examples
 # data.frame method
 factorize_secuTrial.data.frame <- function(data, cl, form){
   if(!is.character(cl$column)) cl$column <- as.character(cl$column)
@@ -34,15 +60,23 @@ factorize_secuTrial.data.frame <- function(data, cl, form){
   return(data)
 }
 
-# integer method
-factorize_secuTrial.integer <- function(data, lookup){
+#' Method for integer objects
+#' @rdname factor
+#' @param var a variable
+#' @param lookup a restricted version of cl (filtered based on \code{form}), containing only the rows relevant for \code{var}
+#'
+#' @export
+#'
+#' @examples
+factorize_secuTrial.integer <- function(var, lookup){
   lookup <- unique(lookup)
   # print(lookup)
-  factor(data, lookup$code, lookup$value)
+  factor(var, lookup$code, lookup$value)
 }
 
-# logical method
-factorize_secuTrial.logical <- function(data, lookup){
-  data <- as.numeric(data)
-  factor(data, lookup$code, lookup$value)
+#' Method for logical objects
+#' @rdname factor
+factorize_secuTrial.logical <- function(var, lookup){
+  var <- as.numeric(var)
+  factor(var, lookup$code, lookup$value)
 }
