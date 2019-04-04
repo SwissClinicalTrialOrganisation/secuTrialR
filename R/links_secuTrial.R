354ebd96bd9edb91c3326bfa4b83a6c7f97e6d1e
aghaynes <aghaynes@gmail.com>
2019-04-04 15:51:57 +0200

    function to generate network map of linkages

---
+++ R/links_secuTrial.R
@@ -0,0 +1,67 @@
+#' Show links between forms
+#'
+#' SecuTrial creates a large number of files and identifiers with which to link files together. Understanding the links can be difficult. This function produces a map linking the forms with common variables.
+#'
+#' @param object secuTrialdata object
+#' @param forms a regular expression for which forms should be included
+#'
+#' @return a tikz plot window. We recommend to resize the window and and click view::fit to screen to improve readability. Forms are coloured red, variables are coloured blue.
+#' @export
+#'
+#' @examples
+#' # prepare path to example export
+#' export_location <- system.file("extdata", "s_export_CSV-xls_BMD.zip",
+#'                                package = "secuTrialR")
+#' # load all export data
+#' sT_export <- load_secuTrial_export(data_dir = export_location)
+#' # get labels
+#' links_secuTrial(sT_export)
+links_secuTrial <- function(object, forms = NULL){
+  if(!class(object) == "secuTrialdata") stop("object of class secuTrialdata expect")
+  obj <- object[2:length(object)]
+
+  names <- lapply(obj, names)
+
+  x <- lapply(names(names), function(x){
+    # print(x)
+    l <- ncol(obj[[x]])
+    rx <- rep(x, l)
+    data.frame(form = rx, var = names(obj[[x]]), stringsAsFactors = FALSE)
+    })
+  x <- do.call("rbind", x)
+  sx <- split(x, x$var)
+  sxid <- mapply(function(x, y){
+    x$q_id <- y
+    x$q_n <- nrow(x)
+    x
+  }, sx, 1:length(unique(x$var)), SIMPLIFY = FALSE)
+  qid <- do.call("rbind", sxid)
+  sx <- split(qid, qid$form)
+  sxid <- mapply(function(x, y){
+    x$f_id <- y
+    x
+  }, sx, 1:length(unique(qid$form)), SIMPLIFY = FALSE)
+  fqid <- do.call("rbind", sxid)
+  if(!is.null(forms)) fqid <- fqid[grepl(forms, fqid$form), ]
+  fqid <- fqid[fqid$q_n > 1, ]
+
+  vs <- data.frame(node = c(fqid$form, fqid$var),
+                   y = rep(1:0, each = nrow(fqid)),
+                   stringsAsFactors = FALSE)
+
+  vs <- vs[!duplicated(vs), ]
+
+  g <- graph_from_data_frame(fqid[, c("form", "var")],
+                             directed = FALSE,
+                             vertices = vs)
+  V(g)$color <- ifelse(V(g)$y == 0, "blue", "red")
+  tkplot(g, layout = layout_as_tree)
+
+}
+
+
+
+
+
+
+
