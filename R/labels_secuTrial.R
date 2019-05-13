a94cebcf1d06df6c45833939f3e9a460a3ef95ee
aghaynes <aghaynes@gmail.com>
2019-05-13 11:37:06 +0200

    New method for labelling (labels variable directly)

--- R/labels_secuTrial.R
+++ R/labels_secuTrial.R
@@ -29,3 +29,84 @@ labels_secuTrial <- function(object, form = NULL) {
   names(it2) <- it$ffcolname
   it2
 }
+
+
+
+
+#' Add labels to variables in secuTrialdata objects
+#' @description
+#' @param x a \code{secuTrialdata} object
+#' @export
+#' @details
+#'
+#' @examples
+#' # load secuTrial export with separate reference table
+#' sT_export <- load_secuTrial_export(system.file("extdata", "s_export_CSV-xls_CTU05_longnames_sep_ref.zip", package = "secuTrialR"))
+#' # label the secuTrialdata object
+#' sT_export_factorized <- label_secuTrial(sT_export)
+label_secuTrial <- function(x, ...) UseMethod("label_secuTrial", x)
+#' @export
+label_secuTrial.secuTrialdata <- function(object) {
+  if (!object$export_options$meta_available$items) {
+    stop("'items' metadata not available")
+  }
+
+  it <- object[[object$export_options$meta_names$items]]
+  w <- sapply(it, class) == "factor"
+  if(any(w)) it[, w] <- sapply(it[, w], as.character)
+  qs <- object[[object$export_options$meta_names$questions]]
+  itqs <- merge(it, qs, by = "fgid")
+  it <- itqs
+  it <- it[!grepl("Dummy", as.character(it$itemtype)), ]
+  # it <- it[, c("ffcolname", "fflabel", "formtablename")]
+  it$formtablename <- as.character(it$formtablename)
+  it$formname <- gsub(pattern = "^mnp", "", it$formtablename)
+  it <- it[!duplicated(it[, c("ffcolname", "fflabel", "formtablename")]), ]
+  # some variables are still duplicated - fflabel can differ
+  # - retain longest
+  it$length <- nchar(it$fflabel)
+  it <- it[order(it$length, decreasing = TRUE), ]
+  it <- it[!duplicated(it[, c("ffcolname", "formtablename")]), ]
+
+  x <- object$export_options$data_names
+  names(x) <- NULL
+  x <- x[!x %in% object$export_options$meta_names]
+  x <- x[x %in% it$formname]
+  # note that the basic form for extended forms might have no variables
+  obs <- lapply(x, function(obj){
+    tmp <- object[[obj]]
+    tmp <- label_secuTrial(tmp, it[it$formname == obj, ])
+    tmp
+  })
+  obs
+  object[x] <- obs
+  object
+}
+
+# nolint start
+# #' The data.frame method is used on the individual datasets within the \code{secuTrialdata} object, and relies on \code{cl}
+# #' @rdname factorize
+# #' @param data a \code{data.frame}, usually from within a \code{secuTrialdata} object
+# #' @param cl a \code{data.frame}, usually taken from a \code{secuTrialdata} object, containing at least three variables - 1) \code{column} containing information on which form and variable is to be factorized (formatted as form.variable); 2) \code{code} containing the options that the variable can take as a number; 3) \code{value} contains the text relating to the \code{code} number
+# #' @param form which form you are currently working on (used for filtering \code{cl})
+# #'
+# #' @examples
+# # data.frame method
+# nolint end
+label_secuTrial.data.frame <- function(data, it) {
+  it <- it[it$ffcolname %in% names(data), ]
+  # print(it)
+  for (i in names(data)[names(data) %in% it$ffcolname]) {
+    x <- it$fflabel[it$ffcolname == i]
+    # print(paste(i, "label", x))
+    Hmisc::label(data[, i]) <- x
+  }
+
+  return(data)
+}
+
+
+
+
+
+
