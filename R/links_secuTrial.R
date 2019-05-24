5c1746d3ade95a77ca02b81c7d4846044d1b22ae
Patrick R. Wright <patrick.wright@usb.ch>
2019-05-24 13:21:57 +0200

    testing CDMA linkage function

--- R/links_secuTrial.R
+++ R/links_secuTrial.R
@@ -77,5 +77,7 @@ links_secuTrial <- function(object, forms = NULL, formcol = "#d8b365", varcol = 
                              vertices = vs)
   igraph::V(g)$color <- ifelse(igraph::V(g)$y == 0, varcol, formcol)
   igraph::tkplot(g, layout = layout_as_tree)
+  # for testing purposes
+  length(unlist(g))
 
 }
