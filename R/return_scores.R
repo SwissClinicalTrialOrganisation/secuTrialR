6382b0a6d12ad27c9400132010f906a72e4b2cd4
Patrick R. Wright <patrick.wright@usb.ch>
2019-05-27 14:47:15 +0200

    adding function to return scores in CDMA

---
+++ R/return_scores.R
@@ -0,0 +1,35 @@
+#' Returns the score (calculated) items from secuTrialdata objects
+#' @description secuTrial allows to set up calculated fields (i.e. scores) that depend on other items. It is not
+#'              suggested to use the scores calculated by secuTrial to perform reliable analyses. To this end,
+#'              calling \code{return_scores} will return all items in the secuTrial export which are scores and should
+#'              be manually recalculated before data analysis.
+#' @param x a \code{secuTrialdata} object
+#' @export
+#' @details return_scores will produce a data.frame that pinpoints which items are scores/calculated.
+#'
+#' @examples
+#' # export location
+#' expot_loc <- system.file("extdata",
+#'                          "s_export_CSV-xls_CTU05_longnames_sep_ref.zip",
+#'                          package = "secuTrialR")
+#' # read export
+#' sT_export <- read_secuTrial(expot_loc)
+#'
+#' # return scores
+#' return_scores(sT_export)
+#'
+return_scores <- function(x) {
+
+  if (class(x) == "secuTrialdata") {
+    curr_items <- data.frame(x[x$export_options$meta_names$items])
+    itemtype_col <- grep("itemtype", names(curr_items))
+    scores <- curr_items[grep("calculated", curr_items[, itemtype_col]), ]
+    scores_relevant_col <- scores[, c(4, 5, 7)]
+    names(scores_relevant_col) <- c("name", "itemtype", "label")
+    rownames(scores_relevant_col) <- NULL
+    scores_relevant_col <- unique(scores_relevant_col)
+    return(scores_relevant_col)
+  } else {
+    stop("return_scores requires objects of the class 'secuTrialdata' as input.")
+  }
+}
