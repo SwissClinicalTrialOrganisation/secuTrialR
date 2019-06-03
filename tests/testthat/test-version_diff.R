c29ba86735512a72d9b789a5602304ae26e0d5ec
Patrick R. Wright <patrick.wright@usb.ch>
2019-06-03 14:55:05 +0200

    make the test work both for windows and linux systems

--- tests/testthat/test-version_diff.R
+++ tests/testthat/test-version_diff.R
@@ -1,8 +1,9 @@
 context("version increment")
 
-return_code <- system("git diff | grep 'Version:'", intern = TRUE)
+return_code <- system("git diff", intern = TRUE)
+grep_version <- grep("Version: \\d\\.\\d\\.\\d", return_code)
 
 # should return length 2 if the Version number has changed
 test_that("Version number has been incremented", {
-  expect_true(length(return_code) > 2)
+  expect_true(length(grep_version) >= 2)
 })
