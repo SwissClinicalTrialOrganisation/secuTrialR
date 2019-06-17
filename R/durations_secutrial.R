52f6490ed89a6113090f82d3a01460b4dd79aa6f
aghaynes <aghaynes@gmail.com>
2019-06-17 11:38:19 +0200

    better approach

--- R/durations_secutrial.R
+++ R/durations_secutrial.R
@@ -19,17 +19,17 @@ durations_secuTrial.secuTrialdata <- function(object){
   x <- x[!x %in% object$export_options$meta_names]
 
   obs <- lapply(x, function(obj){
-    # print(obj)
+    print(obj)
     # find date variables
     it <- object[[object$export_options$meta_names$items]]
     qu <- object[[object$export_options$meta_names$questions]]
     itqu <- merge(it, qu, by = "fgid")
     itqu <- itqu[grepl(obj, as.character(itqu$formtablename)), ]
-    if (nrow(itqu) > 0){ # particularly relevant for audit trail
+    if (nrow(itqu) > 0) { # particularly relevant for audit trail
       itqu$itemtype <- as.character(itqu$itemtype)
       itqu$ffcolname <- as.character(itqu$ffcolname)
 
-      intervals <- itqu[grepl("Interval", itqu$itemtype), ]
+      intervals <- itqu[grepl("calculated only", itqu$itemtype), ] # USE "CALCULATED ONLY"
       intervals <- unique(intervals[, c("ffcolname",
                                         "itemtype",
                                         "fflabel",
@@ -38,12 +38,14 @@ durations_secuTrial.secuTrialdata <- function(object){
         intervals$length <- NA
         intervals$format <- NA
         g <- function(x) grep(x, intervals$itemtype)
-        intervals$length[g("Time .* h-m ")] <- 4
-        intervals$format[g("Time .* h-m ")] <- "h-m"
-        intervals$length[g(" y ")] <- 1 # can ignore?
-        intervals$format[g(" y ")] <- "y"
-        intervals$length[g("Date .* y-m-d-h-m ")] <- 10 # 99-11-30-23-59
-        intervals$format[g("Date .* y-m-d-h-m ")] <- "y-m-d-h-m"
+        intervals$format[g(" m-s ")] <- "m-s"
+        intervals$format[g(" h-m-s ")] <- "h-m-s"
+        intervals$format[g(" h-m ")] <- "h-m"
+        intervals$format[g(" d-h-m ")] <- "d-h-m"
+        intervals$format[g(" y-m-d-h-m ")] <- "y-m-d-h-m"
+        intervals$format[g(" y-m-d ")] <- "y-m-d"
+        intervals$format[g(" y-m ")] <- "y-m"
+        intervals$format[g(" y ")] <- "y" # could probably drop them
       }
 
       g <- function(x) grep(x, times$itemtype)
@@ -54,14 +56,13 @@ durations_secuTrial.secuTrialdata <- function(object){
                                 "fflabel",
                                 "formtablename")])
       if (nrow(times) > 0) {
-        times$length <- NA
         times$format <- NA
-        times$length[g("\\(hh:mm\\)")] <- 4
         times$format[g("\\(hh:mm\\)")] <- "hh:mm"
+        times$format[g("\\(mm:ss\\)")] <- "mm:ss"
       }
 
       tmp <- object[[obj]]
-      tmp <- durations_secuTrial(tmp, intervals, times)
+      tmp <- durations_secuTrial(tmp, intervals = intervals, times = times)
       out <- tmp
     } else {
       out <- object[[obj]]
@@ -82,16 +83,16 @@ durations_secuTrial.data.frame <- function(data,
                                            warn = FALSE,
                                            ...){
   # print(df)
-  intervalvars <- intervals[intervals$ffcolname %in% names(data),]
-  timevars <- times[times$ffcolname %in% names(data),]
+  intervalvars <- intervals[intervals$ffcolname %in% names(data), ]
+  timevars <- times[times$ffcolname %in% names(data), ]
   if (nrow(intervalvars) > 0) {
     # print("interval")
     for (x in 1:nrow(intervalvars)) {
       v <- intervalvars$ffcolname[x]
-      # print(v)
+      print(paste(" ", v))
       l <- intervalvars$length[x]
       f <- intervalvars$format[x]
-      newcol <- durations_secuTrial(data[, v], l, f, ...)
+      newcol <- durations_secuTrial(data[, v], f, ...)
       nv <- paste0(v, ".dur")
       data[, nv] <- newcol
       label(data[, nv]) <- paste0(label(data[, x]), " (smallest unit ", f, ")")
@@ -104,10 +105,11 @@ durations_secuTrial.data.frame <- function(data,
     # print("time")
     for (x in 1:nrow(timevars)) {
       v <- timevars$ffcolname[x]
+      print(paste(" ", v))
       # print(v)
       l <- timevars$length[x]
       f <- timevars$format[x]
-      newcol <- times_secuTrial(data[, v], l, f)
+      newcol <- times_secuTrial(data[, v], f)
       nv <- paste0(v, ".time")
       data[, nv] <- newcol
       label(data[, nv]) <- paste0(label(data[, x]), " (clock time)")
@@ -124,28 +126,21 @@ durations_secuTrial.data.frame <- function(data,
 
 
 durations_secuTrial.numeric <- function(var,
-                                        length,
                                         format,
-                                        days_in_year = 365){
-  days_in_month <- days_in_year/12
+                                        ...){
+  f2l <- format2length(format, ...)
   # print("integer")
   # print(length)
   # print(format)
-  if (length > 1){
+  if (f2l$length > 1){
     # pad
-    z <- sprintf(paste0("%0", length, ".0f"), var)
+    z <- sprintf(paste0("%0", f2l$length, ".0f"), var)
     z[is.na(var)] <- NA
     l <- lapply(z, function(x){
-      s <- substring(x, seq(1, length, 2), seq(2, length, 2))
+      s <- substring(x, f2l$cp1, f2l$cp2)
       as.numeric(s)
     })
-    if (format == "h-m") multiplier <- c(60, 1)
-    if (format == "y-m-d-h-m") multiplier <- c(days_in_year*24*60, # years -> mins
-                                               days_in_month*24*60, # months -> mins
-                                               24*60, # days -> mins
-                                               60, # hours -> mins
-                                               1) # mins
-    out <- sapply(l, function(x) sum(x * multiplier))
+    out <- sapply(l, function(x) sum(x * f2l$multiplier))
   } else {
     out <- var
   }
@@ -154,19 +149,57 @@ durations_secuTrial.numeric <- function(var,
 
 
 times_secuTrial <- function(var,
-                            length,
                             format){
+  f2l <- format2length(format)
   # print("times")
   if (length(var) > 0) {
-    z <- sprintf(paste0("%0", length, ".0f"), var)
+    z <- sprintf(paste0("%0", f2l$length, ".0f"), var)
     z[is.na(var)] <- NA
     l <- lapply(z, function(x){
-      s <- substring(x, seq(1, length, 2), seq(2, length, 2))
+      s <- substring(x, seq(1, f2l$length, 2), seq(2, f2l$length, 2))
     })
     # need others?
     out <- sapply(l, paste0, collapse = ":")
+    out[is.na(var)] <- NA
   } else {
     out <- var
   }
   out
 }
+
+
+format2length <- function(x, days_in_year = 365){
+  days_in_month <- days_in_year / 12
+  fs <- data.frame(format = c("m-s", "h-m-s", "h-m", "d-h-m", "y-m-d-h-m", "y-m-d", "y-m", "y", "hh:mm", "mm:ss"),
+                   length = c(4, 6, 4, 6, 12, 8, 6, 4, 4, 4),
+                   cut1 = c(2, 2, 2, 2, 4, 4, 4, 4, 2, 2),
+                   stringsAsFactors = FALSE)
+  w <- match(x, fs$format)
+  fs <- fs[w, ]
+  if (fs$cut1 < fs$length){
+    cp1 <- c(1, seq(fs$cut1+1, fs$length, 2))
+    cp2 <- c(fs$cut1, seq(fs$cut1+2, fs$length, 2))
+  } else {
+    cp1 <- 1
+    cp2 <- fs$cut1
+  }
+  # multipliers
+  if (x == "m-s") multiplier <- c(60, 1)
+  if (x == "h-m-s") multiplier <- c(60^2, 60, 1)
+  if (x == "h-m") multiplier <- c(60, 1)
+  if (x == "d-h-m") multiplier <- c(24*60, 60, 1)
+  if (x == "y-m-d-h-m") multiplier <- c(days_in_year*24*60, days_in_month*24*60, 24*60, 60, 1)
+  if (x == "y-m-d") multiplier <- c(days_in_year, days_in_month, 1)
+  if (x == "y-m") multiplier <- c(12, 1)
+  if (x == "y") multiplier <- c(1)
+  if (x == "hh:mm") multiplier <- NA
+  if (x == "mm:ss") multiplier <- NA
+  return(list(format = fs$format,
+              length = fs$length,
+              cut1 = fs$cut1,
+              cp1 = cp1,
+              cp2 = cp2,
+              multiplier = multiplier))
+}
+
+
