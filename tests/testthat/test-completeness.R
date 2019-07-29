7b4ecb9ce974ea0c6260c08f0c247cd5b2691e57
Patrick R. Wright <patrick.wright@usb.ch>
2019-07-29 16:29:59 +0200

    more tests including manual validation against web interface of secuTrial

--- tests/testthat/test-completeness.R
+++ tests/testthat/test-completeness.R
@@ -23,11 +23,45 @@ test_that("Test fail", {
 })
 
 # long and short cannot match on form_names, so we just check the data columns
-cols <- c("pat_id", "completely filled", "partly filled", "empty", "with warnings", "with errors")
+cols_counts <- c("pat_id", "completely filled", "partly filled", "empty", "with warnings", "with errors")
 
 test_that("Test output equality for different export options", {
-  expect_equal(form_status_counts(s_ctu05)[, cols], form_status_counts(l_ctu05)[, cols])
-  expect_equal(form_status_counts(s_tes05_iso)[, cols], form_status_counts(l_tes05_utf)[, cols])
+  expect_equal(form_status_counts(s_ctu05)[, cols_counts], form_status_counts(l_ctu05)[, cols_counts])
+  expect_equal(form_status_counts(s_tes05_iso)[, cols_counts], form_status_counts(l_tes05_utf)[, cols_counts])
+})
+
+test_that("Test column sums", {
+  expect_equal(as.vector(colSums(form_status_counts(l_ctu05)[,3:7])), c(74, 5, 0, 0, 0))
+  expect_equal(as.vector(colSums(form_status_counts(s_tes05_iso)[,3:7])), c(21, 12, 4, 0, 0))
+})
+
+# custom count checks
+# as manually compared to the secuTrial web interface
+counts_for_custom_tests <- form_status_counts(s_tes05_iso)
+test_that("Individual entries", {
+  # RPACKRIG-USZ-11111 has 4 (1x baseline, 3x fu visit) empty forms and nothing is filled at all
+  expect_equal(counts_for_custom_tests[which(counts_for_custom_tests$pat_id == "RPACKRIG-USZ-11111" &
+                                  counts_for_custom_tests$form_name == "bl"), ]$empty,
+               1)
+  expect_equal(counts_for_custom_tests[which(counts_for_custom_tests$pat_id == "RPACKRIG-USZ-11111" &
+                                             counts_for_custom_tests$form_name == "fuvisit"), ]$empty,
+               3)
+  # RPACKRIG-USZ-4 has 1x baseline completely filled,
+  #                    3x fu visit completely filled,
+  #                    1x fu visit partly filled,
+  #                    1x intervals completely filled
+  expect_equal(counts_for_custom_tests[which(counts_for_custom_tests$pat_id == "RPACKRIG-USZ-4" &
+                                               counts_for_custom_tests$form_name == "bl"), ]$`completely filled`,
+               1)
+  expect_equal(counts_for_custom_tests[which(counts_for_custom_tests$pat_id == "RPACKRIG-USZ-4" &
+                                               counts_for_custom_tests$form_name == "fuvisit"), ]$`completely filled`,
+               3)
+  expect_equal(counts_for_custom_tests[which(counts_for_custom_tests$pat_id == "RPACKRIG-USZ-4" &
+                                               counts_for_custom_tests$form_name == "fuvisit"), ]$`partly filled`,
+               1)
+  expect_equal(counts_for_custom_tests[which(counts_for_custom_tests$pat_id == "RPACKRIG-USZ-4" &
+                                               counts_for_custom_tests$form_name == "intervals"), ]$`completely filled`,
+               1)
 })
 
 test_that("Test that partly, completely and empty percentages add up to 1 i.e. 100%", {
@@ -44,5 +78,20 @@ test_that("Test that partly, completely and empty percentages add up to 1 i.e. 1
                0)
 })
 
+cols_summary <- c("partly filled", "completely filled", "empty", "with warnings",
+                  "with errors", "partly filled.percent", "completely filled.percent",
+                  "empty.percent", "with warnings.percent", "with errors.percent", "form_count")
 
-# TODO add tests with warnings and errors and empty data
+test_that("Test column sums", {
+  expect_equal(colSums(form_status_summary(s_ctu05)[, cols_summary]),
+               colSums(form_status_summary(l_ctu05)[, cols_summary]))
+  expect_equal(round(as.vector(colSums(form_status_summary(l_ctu05)[, cols_summary])), digits = 4),
+               c(5, 74, 0, 0, 0, 0.3122, 9.6878, 0, 0, 0, 79))
+  expect_equal(colSums(form_status_summary(s_tes05_iso)[, cols_summary]),
+               colSums(form_status_summary(l_tes05_utf)[, cols_summary]))
+  expect_equal(round(as.vector(colSums(form_status_summary(s_tes05_iso)[, cols_summary])), digits = 4),
+               c(12, 21, 4, 0, 0, 2.9774, 2.6798, 0.3429, 0, 0, 37)
+               )
+})
+
+# TODO add more tests with warnings and errors and empty data
