63b8ec3e86ab9012adbcf3304a573d9569cbaa64
aghaynes <aghaynes@gmail.com>
2019-08-21 13:56:24 +0200

    some tests for visit_structure

--- tests/testthat/test-visit_structure.R
+++ tests/testthat/test-visit_structure.R
@@ -32,3 +32,34 @@ test_that("failure on CTU05 data", {
   expect_error(visit_structure(sdat))
   expect_error(visit_structure(ldat))
 })
+
+dat <- read_secuTrial(system.file("extdata",
+                                  "s_export_CSV-xls_TES05_short_ISO-8859-15.zip",
+                                  package = "secuTrialR"))
+
+test_that("no fail with TES05", {
+          expect_error(visit_structure(dat), regexp = NA)
+          expect_error(plot(visit_structure(dat)), regexp = NA)
+})
+
+vs <- visit_structure(dat)
+test_that("correct dims", {
+  expect_equal(dim(vs), c(3,6))
+})
+
+test_that("all forms included", {
+  expect_true(all(unique(as.character(dat$fs$formname)) %in% rownames(vs)))
+  expect_true(all(rownames(vs) %in% unique(as.character(dat$fs$formname))))
+})
+
+test_that("all visits included", {
+  expect_true(all(unique(as.character(dat$vp$mnpvislabel)) %in% names(vs)))
+  expect_true(all(names(vs)[-1] %in% unique(as.character(dat$vp$mnpvislabel))))
+})
+
+test_that("colsums, rowsums correct", {
+  cs <- colSums(vs[, -1], na.rm = TRUE)
+  expect_equal(as.numeric(cs), c(1,1,1,1,2))
+  rs <- rowSums(vs[, -1], na.rm = TRUE)
+  expect_equal(as.numeric(rs), c(1,4,1))
+})
