6bdf97426796a07f1395099e0a242691b28f9404
aghaynes <aghaynes@gmail.com>
2019-08-21 16:07:15 +0200

    metadata doesnt count either

--- tests/testthat/test-links_secuTrial.R
+++ tests/testthat/test-links_secuTrial.R
@@ -32,6 +32,6 @@ len_sep_ref <- links_secuTrial(sT_export_sep_ref, plot = FALSE)
 
 test_that("igraph structure", {
   expect_equal(len_short, len_short)
-  expect_equal(len_short, 375)
-  expect_equal(len_sep_ref, 1322)
+  expect_equal(len_short, 310)
+  expect_equal(len_sep_ref, 1266)
 })
