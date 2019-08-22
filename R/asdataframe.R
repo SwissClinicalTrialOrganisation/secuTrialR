c69402b0f98ab23884dafe64acf1067cb0032f65
aghaynes <aghaynes@gmail.com>
2019-08-22 10:47:58 +0200

    add checks

--- R/asdataframe.R
+++ R/asdataframe.R
@@ -1,8 +1,10 @@
 #' as.data.frame method for secuTrialdata objects
 #'
 #' @param object secuTrialdata object
+#' @param data.frames character vector of data.frame names to turn into data.frames
 #' @param meta logical should metadata be returned
-#' @param rm_regex regex syntax to remove from names
+#' @param regex regex syntax to remove from names
+#' @param rep replacement for regex
 #' @param envir environment in which to put the data
 #'
 #' @return each data.frame on the secuTrialdata object is saved to it's own data.frame in the environment
@@ -19,15 +21,26 @@
 #' as.data.frame(sT_export)
 #' # add files to global environment, removing the project name from the file names
 #' as.data.frame(sT_export, rm_regex = "ctu05")
-as.data.frame.secuTrialdata <- function(object, meta = TRUE, rm_regex = NULL, envir = .GlobalEnv) {
+as.data.frame.secuTrialdata <- function(object,
+                                        data.frames = NULL,
+                                        meta = TRUE,
+                                        regex = NULL,
+                                        rep = "",
+                                        envir = .GlobalEnv) {
+
+  if (all(!is.character(regex), !is.null(regex))) stop("regex should be either NULL or character")
+  if (!is.character(rep)) stop("rep should be character")
+  if (!is.environment(envir)) stop("envir should be an environment")
 
   datanames <- object$export_options$data_names
   datanames <- as.character(datanames)
+  if (!all(data.frames %in% data.names)) stop("unrecognised data.frame specified")
   if (!meta) {
     datanames <- datanames[!datanames %in% unlist(object$export_options$meta_names)]
   }
+  if (!is.null(data.frames)) datanames <- datanames[datanames %in% data.frames]
   datanames2 <- datanames
-  if (!is.null(rm_regex)) datanames2 <- gsub(rm_regex, "", datanames)
+  if (!is.null(regex)) datanames2 <- gsub(regex, rep, datanames)
 
   invisible(
     mapply(
