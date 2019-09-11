1ae63a9c53dc1987628e836d32f5dac1d1e6c750
markomi <milica.markovic@usb.ch>
2019-09-11 16:34:00 +0200

    renamed test

--- tests/testthat/test-build_secuTrial_url.R
+++ tests/testthat/test-build_secuTrial_url.R
@@ -9,10 +9,11 @@ projid <- "1234"
 docid <- "5678"
 
 # expected build_secuTrial_url() outputs
-sT_url_full <- "https://server.secutrial.com/apps/WebObjects/INSTANCE-01.woa/wa/choose?customer=CUSTOMER-01&projectid=1234&docid=5678"
+sT_url_full <- paste0("https://server.secutrial.com/apps/WebObjects/INSTANCE-01.woa/wa/",
+                      "choose?customer=CUSTOMER-01&projectid=1234&docid=5678")
 sT_url_customer <- "https://server.secutrial.com/apps/WebObjects/INSTANCE-01.woa/wa/choose?customer=CUSTOMER-01"
 
-test_that("Test fail", {
+test_that("All urls built correctly", {
   expect_equal(build_secuTrial_url(server, instance, customer, projid, docid), sT_url_full)
   expect_equal(build_secuTrial_url(server2, instance, customer, projid, docid), sT_url_full)
   expect_equal(build_secuTrial_url(server2, instance, customer, docid = docid), sT_url_customer)
