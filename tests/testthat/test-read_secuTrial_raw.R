3d3e8b2eeafa9aea9bd7074acf2a33bd22b4cfa1
Patrick R. Wright <patrick.wright@usb.ch>
2019-10-03 16:49:46 +0200

    missing add id tests

--- tests/testthat/test-read_secuTrial_raw.R
+++ tests/testthat/test-read_secuTrial_raw.R
@@ -8,17 +8,35 @@ long_export_location <- system.file("extdata", "sT_exports", "BMD",
                                     "s_export_CSV-xls_BMD_long_en_utf8.zip",
                                     package = "secuTrialR")
 
+no_add_id_export_location <- system.file("extdata", "sT_exports", "export_options",
+                                         "s_export_CSV-xls_CTU05_20191003-144833_no_addid.zip",
+                                         package = "secuTrialR")
+
+all_info_export_location <- system.file("extdata", "sT_exports", "export_options",
+                                        "s_export_CSV-xls_CTU05_20191003-144349_all_info.zip",
+                                        package = "secuTrialR")
+
+
 sT_export_short <- read_secuTrial_raw(data_dir = short_export_location)
 sT_export_long <- read_secuTrial_raw(data_dir = long_export_location)
+sT_export_no_add_id <- read_secuTrial_raw(data_dir = no_add_id_export_location)
+sT_export_all_info <- read_secuTrial_raw(data_dir = all_info_export_location)
 
 # test length of list
 test_that("All data tables loaded.", {
   expect_equal(length(names(sT_export_short)), 14)
   expect_equal(length(names(sT_export_long)), 14)
+  expect_equal(length(names(sT_export_no_add_id)), 32)
+  expect_equal(length(names(sT_export_all_info)), 32)
 })
 
 # check dimensions of loaded data
 test_that("Data dimensions are correct.", {
+  # dim CTU05
+  # 62 because everything is available
+  expect_equal(dim(sT_export_all_info$baseline), c(17, 62))
+  # 61 because pat_id has not been added
+  expect_equal(dim(sT_export_no_add_id$baseline), c(17, 61))
   # dim bone mineral density
   expect_equal(dim(sT_export_short$bmd), dim(sT_export_long$dem00bmd))
   expect_equal(dim(sT_export_long$dem00bmd), c(504, 27))
@@ -30,14 +48,10 @@ test_that("Data dimensions are correct.", {
   expect_equal(dim(sT_export_short$cn), c(113, 13))
 })
 
-# check amount of loaded tables
-test_that("Correct number of tables loaded.", {
-  expect_equal(length(names(sT_export_short)), 14)
-  expect_equal(length(names(sT_export_long)), 14)
-})
-
 # check add_id and lab_id
 test_that("add_id and lab_id correctly determined", {
+  expect_true(sT_export_all_info$export_options$add_id)
+  expect_false(sT_export_no_add_id$export_options$add_id)
   expect_true(sT_export_short$export_options$add_id)
   expect_true(sT_export_long$export_options$add_id)
   expect_false(sT_export_short$export_options$lab_id)
