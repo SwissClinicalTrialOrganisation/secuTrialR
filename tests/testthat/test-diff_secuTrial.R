b3d61f84539777337db54a81c17ea3fdd135905e
Patrick R. Wright <patrick.wright@usb.ch>
2019-10-31 11:22:11 +0100

    tests with changes

--- tests/testthat/test-diff_secuTrial.R
+++ tests/testthat/test-diff_secuTrial.R
@@ -22,6 +22,19 @@ bmd <- read_secuTrial(system.file("extdata", "sT_exports", "BMD",
                                   "s_export_CSV-xls_BMD_short_en_utf8.zip",
                                   package = "secuTrialR"))
 
+# v1 is essentially a clone of the CTU05 setup
+ctu06_v1 <- read_secuTrial(system.file("extdata", "sT_exports", "change_tracking",
+                                       "s_export_CSV-xls_CTU06_version1.zip",
+                                       package = "secuTrialR"))
+# v2 contains 2 additional forms (mnpctu06anewform, mnpctu06anothernewform) and 2 additional variables (new_item_in_fu, new_item_in_new_form)
+ctu06_v2 <- read_secuTrial(system.file("extdata", "sT_exports", "change_tracking",
+                                       "s_export_CSV-xls_CTU06_version2.zip",
+                                       package = "secuTrialR"))
+# v3 contains the changes in v2 and add one more variable (yet_another_item)
+ctu06_v3 <- read_secuTrial(system.file("extdata", "sT_exports", "change_tracking",
+                                       "s_export_CSV-xls_CTU06_version3.zip",
+                                       package = "secuTrialR"))
+
 test_that("Errors", {
   expect_error(diff_secuTrial("asd", bmd))
   expect_error(diff_secuTrial(ctu05_en_1, bmd))
@@ -38,4 +51,19 @@ test_that("Same version", {
 test_that("Different version", {
   expect_equal(length(diff_secuTrial(ctu05_en_1, ctu05_fr)[[1]]), 0)
   expect_equal(length(diff_secuTrial(ctu05_en_1, ctu05_fr)[[2]]), 0)
+})
+
+
+# different versions with changes
+v1_vs_v2 <- diff_secuTrial(ctu06_v1, ctu06_v2)
+v1_vs_v3 <- diff_secuTrial(ctu06_v1, ctu06_v3)
+v2_vs_v3 <- diff_secuTrial(ctu06_v2, ctu06_v3)
+
+test_that("Different version and changes", {
+  expect_equal(v1_vs_v2$new_forms, c("mnpctu06anewform", "mnpctu06anothernewform"))
+  expect_equal(v1_vs_v2$new_variables, c("new_item_in_fu", "new_item_in_new_form"))
+  expect_equal(v1_vs_v3$new_forms, c("mnpctu06anewform", "mnpctu06anothernewform"))
+  expect_equal(v1_vs_v3$new_variables, c("yet_another_item", "new_item_in_fu", "new_item_in_new_form"))
+  expect_equal(length(v2_vs_v3$new_forms), 0)
+  expect_equal(v2_vs_v3$new_variables, "yet_another_item")
 })
