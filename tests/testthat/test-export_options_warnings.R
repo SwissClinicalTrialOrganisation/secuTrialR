e3653f66626dcaf124624f004a9423fdec0ce773
Patrick R. Wright <patrick.wright@usb.ch>
2019-11-14 16:16:07 +0100

    add check_export_options function

---
+++ tests/testthat/test-export_options_warnings.R
@@ -0,0 +1,23 @@
+context("export options warning")
+
+path_only_col_names <- system.file("extdata", "sT_exports", "export_options",
+                                   "s_export_CSV-xls_CTU05_20191004-131608_only_column_names.zip",
+                                   package = "secuTrialR")
+sT_only_col_names <- read_secuTrial_raw(path_only_col_names)
+
+path_all_info <- system.file("extdata", "sT_exports", "export_options",
+                             "s_export_CSV-xls_CTU05_20191003-144349_all_info.zip",
+                             package = "secuTrialR")
+sT_all_info <- read_secuTrial_raw(path_all_info)
+
+path_unzipped <- system.file("extdata", "sT_exports", "BMD",
+                             "s_export_CSV-xls_BMD_short_en_utf8",
+                             package = "secuTrialR")
+sT_unzipped <- read_secuTrial_raw(path_unzipped)
+
+test_that("error, warning and no warning", {
+  expect_error(check_export_options(1337))
+  expect_warning(check_export_options(sT_unzipped))
+  expect_warning(check_export_options(sT_only_col_names))
+  expect_equal(check_export_options(sT_all_info), NULL)
+})
