fe8e343846e1a8ed4715269314007c9aa6544e90
Patrick R. Wright <patrick.wright@usb.ch>
2019-11-21 18:00:33 +0100

    download handling - will need modularization

--- inst/shinyApp/app.R
+++ inst/shinyApp/app.R
@@ -26,7 +26,7 @@ ui <- dashboardPage(skin = "red",
                         menuItem("Visit plan", tabName = "visitstructure", icon = icon("calendar-alt")),
                         menuItem("Monitoring cases", tabName = "moncases", icon = icon("dice")),
                         menuItem("Codebook", tabName = "codebook", icon = icon("book")),
-                        menuItem("Download", tabName = "download", icon = icon("download"))
+                        menuItem("STATA - SAS - SPSS", tabName = "download", icon = icon("download"))
                       )
                     ),
                     dashboardBody(
@@ -51,7 +51,8 @@ ui <- dashboardPage(skin = "red",
                         # Second tab content
                         tabItem(tabName = "recruitmentplot",
                                 h2("Study recruitment"),
-                                box(plotOutput("recruitment_plot", height = 500, width = 900), width = 1000)
+                                box(plotOutput("recruitment_plot", height = 500, width = 900), width = 1000),
+                                downloadButton("downloadDataRecruitmentPlot", "Download pdf")
                         ),
                         # Third tab content
                         tabItem(tabName = "recruitmenttable",
@@ -111,13 +112,21 @@ ui <- dashboardPage(skin = "red",
 
                         # Last tab content / Download
                         tabItem(tabName = "download",
-                                h2("Download files"),
-                                h4("Download recruitment plot"),
-                                downloadButton("downloadDataRecruitmentPlot", "Download"),
-                                h4("Download Stata conversion"),
-                                downloadButton("downloadDataStata", "Download")
-                                #h4("Download plots"),
-                                #downloadButton('downloadData', 'Download')
+                                h2("Download data conversion archives"),
+                                br(),
+                                h4("Download STATA"),
+                                downloadButton("downloadDataStata", "Download dta zip archive"),
+                                br(),
+                                br(),
+                                h4("Download SAS"),
+                                downloadButton("downloadDataSas", "Download sas7bdat zip archive"),
+                                br(),
+                                br(),
+                                downloadButton("downloadDataXpt", "Download xpt v8 zip archive"),
+                                br(),
+                                br(),
+                                h4("Download SPSS"),
+                                downloadButton("downloadDataSav", "Download sav zip archive")
                         )
                       )
                     )
@@ -274,22 +283,72 @@ server <- function(input, output, session) {
   )
 
   output$downloadDataStata <- downloadHandler(
-
     tdir <- tempdir(),
-
     # This function returns a string which tells the client
     # browser what name to use when saving the file.
     filename = function() {
       "stata.zip"
     },
-
     # This function should write data to a file given to it by
     # the argument 'file'.
     content = function(file) {
       # Write to a file specified by the 'file' argument
-
       write_secuTrial(sT_export(), format = "dta", path = tdir)
       dta_loc <- list.files(path = tdir, full.names = TRUE, pattern = "dta$")
+      # -j prevents keeping directory structure
+      zip(zipfile = file, files = dta_loc, flags = "-j")
+    }
+  )
+
+  output$downloadDataSas <- downloadHandler(
+    tdir <- tempdir(),
+    # This function returns a string which tells the client
+    # browser what name to use when saving the file.
+    filename = function() {
+      "sas7bdat.zip"
+    },
+    # This function should write data to a file given to it by
+    # the argument 'file'.
+    content = function(file) {
+      # Write to a file specified by the 'file' argument
+      write_secuTrial(sT_export(), format = "sas", path = tdir)
+      dta_loc <- list.files(path = tdir, full.names = TRUE, pattern = "sas7bdat$")
+      # -j prevents keeping directory structure
+      zip(zipfile = file, files = dta_loc, flags = "-j")
+    }
+  )
+
+  output$downloadDataXpt <- downloadHandler(
+    tdir <- tempdir(),
+    # This function returns a string which tells the client
+    # browser what name to use when saving the file.
+    filename = function() {
+      "xpt.zip"
+    },
+    # This function should write data to a file given to it by
+    # the argument 'file'.
+    content = function(file) {
+      # Write to a file specified by the 'file' argument
+      write_secuTrial(sT_export(), format = "xpt", path = tdir)
+      dta_loc <- list.files(path = tdir, full.names = TRUE, pattern = "xpt$")
+      # -j prevents keeping directory structure
+      zip(zipfile = file, files = dta_loc, flags = "-j")
+    }
+  )
+
+  output$downloadDataSav <- downloadHandler(
+    tdir <- tempdir(),
+    # This function returns a string which tells the client
+    # browser what name to use when saving the file.
+    filename = function() {
+      "sav.zip"
+    },
+    # This function should write data to a file given to it by
+    # the argument 'file'.
+    content = function(file) {
+      # Write to a file specified by the 'file' argument
+      write_secuTrial(sT_export(), format = "sav", path = tdir)
+      dta_loc <- list.files(path = tdir, full.names = TRUE, pattern = "sav$")
       # -j prevents keeping directory structure
       zip(zipfile = file, files = dta_loc, flags = "-j")
     }
