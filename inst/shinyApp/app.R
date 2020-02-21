387830f904bd29c686cfb349beda843a463058a3
markomi <milica.markovic@usb.ch>
2020-02-21 17:48:50 +0100

    export module implemented

--- inst/shinyApp/app.R
+++ inst/shinyApp/app.R
@@ -47,8 +47,7 @@ ui <- dashboardPage(skin = "red",
                         menuItem("Visit plan", tabName = mod$visitplan, icon = icon("calendar-alt")),
                         menuItem("Monitoring cases", tabName = mod$monitorcn, icon = icon("dice")),
                         menuItem("Codebook", tabName = mod$codebook, icon = icon("book")),
-
-                        menuItem("STATA - SAS - SPSS", tabName = "download", icon = icon("download"))
+                        menuItem("STATA - SAS - SPSS", tabName = mod$export, icon = icon("download"))
                       )
                     ),
                     dashboardBody(
@@ -60,26 +59,7 @@ ui <- dashboardPage(skin = "red",
                         mod_visitplan_UI(mod$visitplan, label = mod$visitplan),
                         mod_monitorcn_UI(mod$monitorcn, label = mod$monitorcn),
                         mod_codebook_UI(mod$codebook, label = mod$codebook),
-
-
-                        # Last tab content / Download
-                        tabItem(tabName = "download",
-                                h2("Download data conversion archives"),
-                                br(),
-                                h4("Download STATA"),
-                                downloadButton("downloadDataStata", "Download dta zip archive"),
-                                br(),
-                                br(),
-                                h4("Download SAS"),
-                                downloadButton("downloadDataSas", "Download sas7bdat zip archive"),
-                                br(),
-                                br(),
-                                downloadButton("downloadDataXpt", "Download xpt v8 zip archive"),
-                                br(),
-                                br(),
-                                h4("Download SPSS"),
-                                downloadButton("downloadDataSav", "Download sav zip archive")
-                        )
+                        mod_export_UI(mod$export, label = mod$export)
                       )
                     )
 )
@@ -94,84 +74,7 @@ server <- function(input, output, session) {
   callModule(mod_visitplan, mod$visitplan, sT_export)
   callModule(mod_monitorcn, mod$monitorcn, sT_export)
   callModule(mod_codebook, mod$codebook, sT_export)
-
-
-
-
-
-
-  output$downloadDataStata <- downloadHandler(
-    tdir <- tempdir(),
-    # This function returns a string which tells the client
-    # browser what name to use when saving the file.
-    filename = function() {
-      "stata.zip"
-    },
-    # This function should write data to a file given to it by
-    # the argument 'file'.
-    content = function(file) {
-      # Write to a file specified by the 'file' argument
-      write_secuTrial(sT_export(), format = "dta", path = tdir)
-      dta_loc <- list.files(path = tdir, full.names = TRUE, pattern = "dta$")
-      # -j prevents keeping directory structure
-      zip(zipfile = file, files = dta_loc, flags = "-j")
-    }
-  )
-
-  output$downloadDataSas <- downloadHandler(
-    tdir <- tempdir(),
-    # This function returns a string which tells the client
-    # browser what name to use when saving the file.
-    filename = function() {
-      "sas7bdat.zip"
-    },
-    # This function should write data to a file given to it by
-    # the argument 'file'.
-    content = function(file) {
-      # Write to a file specified by the 'file' argument
-      write_secuTrial(sT_export(), format = "sas", path = tdir)
-      dta_loc <- list.files(path = tdir, full.names = TRUE, pattern = "sas7bdat$")
-      # -j prevents keeping directory structure
-      zip(zipfile = file, files = dta_loc, flags = "-j")
-    }
-  )
-
-  output$downloadDataXpt <- downloadHandler(
-    tdir <- tempdir(),
-    # This function returns a string which tells the client
-    # browser what name to use when saving the file.
-    filename = function() {
-      "xpt.zip"
-    },
-    # This function should write data to a file given to it by
-    # the argument 'file'.
-    content = function(file) {
-      # Write to a file specified by the 'file' argument
-      write_secuTrial(sT_export(), format = "xpt", path = tdir)
-      dta_loc <- list.files(path = tdir, full.names = TRUE, pattern = "xpt$")
-      # -j prevents keeping directory structure
-      zip(zipfile = file, files = dta_loc, flags = "-j")
-    }
-  )
-
-  output$downloadDataSav <- downloadHandler(
-    tdir <- tempdir(),
-    # This function returns a string which tells the client
-    # browser what name to use when saving the file.
-    filename = function() {
-      "sav.zip"
-    },
-    # This function should write data to a file given to it by
-    # the argument 'file'.
-    content = function(file) {
-      # Write to a file specified by the 'file' argument
-      write_secuTrial(sT_export(), format = "sav", path = tdir)
-      dta_loc <- list.files(path = tdir, full.names = TRUE, pattern = "sav$")
-      # -j prevents keeping directory structure
-      zip(zipfile = file, files = dta_loc, flags = "-j")
-    }
-  )
-
+  callModule(mod_export, mod$export, sT_export)
 }
 
 shinyApp(ui, server)
