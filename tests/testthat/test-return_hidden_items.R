cef1b08d41f5c270fb12a9e28e85e29259b42c6d
Patrick R. Wright <patrick.wright@usb.ch>
2020-03-06 12:25:24 +0100

    add return_hidden_items function

---
+++ tests/testthat/test-return_hidden_items.R
@@ -0,0 +1,25 @@
+context("return hidden items")
+
+sdat <- read_secuTrial(system.file("extdata", "sT_exports", "shortnames",
+                                   "s_export_CSV-xls_CTU05_short_miss_en_utf8.zip",
+                                   package = "secuTrialR"))
+ldat <- read_secuTrial(system.file("extdata", "sT_exports", "longnames",
+                                  "s_export_CSV-xls_CTU05_long_miss_en_utf8.zip",
+                                  package = "secuTrialR"))
+
+export_location_only_col_names <- system.file("extdata", "sT_exports", "export_options",
+                                              "s_export_CSV-xls_CTU05_20191004-131608_only_column_names.zip",
+                                              package = "secuTrialR")
+
+sT_export_only_col_names <- read_secuTrial_raw(data_dir = export_location_only_col_names)
+
+
+test_that("Test fail", {
+  expect_error(return_hidden_items(1337))
+  expect_error(return_hidden_items(sT_export_only_col_names))
+})
+
+test_that("Test output", {
+  expect_equal(return_hidden_items(sdat), return_hidden_items(ldat))
+  expect_equal(nrow(return_hidden_items(ldat)), 0)
+})
